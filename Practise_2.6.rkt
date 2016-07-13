ce045c516140f7a116b9fdf9e0038497e38475ab
kyon <kyon0304@gmail.com>
2016-07-13 17:28:14 +0800

    [bugfix] practise 2.6

--- Practise_2.6.rkt
+++ Practise_2.6.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 (define zero
-  (labmda (f) (lambda (x) x)))
+  (lambda (f) (lambda (x) x)))
 
 (define (add-1 n)
   (lambda (f) (lambda (x) (f ((n f) x)))))
@@ -12,5 +12,15 @@
 (define two
   (lambda (f) (lambda (x) (f (f x)))))
 
-(define (+ a b)
-  (lambda (f) (lambda (x) ((b (a f)) x))))
+(define (add a b)
+  (lambda (f) (lambda (x) ((a f) ((b f) x)))))
+
+((one sqr) 2)
+
+(((add-1 one) sqr) 2)
+
+((two sqr) 2)
+
+(((add-1 two) sqr) 2)
+
+(((add one two) sqr) 2)
